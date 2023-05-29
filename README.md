82c02e6f8f0210379e18e81d1f41c027e4efd032
BlckTitan <dorshman406@gmail.com>
2023-05-29 13:47:33 +0100

    Basic project setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mac
+# Figma template
+https://www.figma.com/community/file/1157809003868202004/Ecommerce-Web-Pages-(Free)?preview=fullscreen
---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-5xl text-teal-600'>Mac</h1>
+    </div>
+  );
+}
+
+export default App;
