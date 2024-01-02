e01392adfba98a4f1bbf6b7bdcb566ebe36377d8
BlckTitan <dorshman406@gmail.com>
2024-01-02 13:29:43 +0100

    Setting up the basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./layout";
+//pages
+import Home from './pages/home';
+import Dashboard from "./pages/dashboard";
+import NoPage from './pages/noPage';
 
 function App() {
   return (
     <div className="App">
-      home
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Dashboard />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
