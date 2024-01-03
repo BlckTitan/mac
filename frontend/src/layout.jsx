98baa03d34e4be9f5cf3dc3bac96da1d2bdf9e58
BlckTitan <dorshman406@gmail.com>
2024-01-03 15:29:01 +0100

    Added a custom color 'regal-blue' Changed the layout for the dashboard and homepage relocated the nav to homepage

--- frontend/src/layout.jsx
+++ frontend/src/layout.jsx
@@ -1,23 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className='relative w-full h-screen'>
-      <nav className="fixed top-0 z-10 w-full h-fit p-4 xl:py-6 px-12 bg-blue-500 xl:text-xl xl:font text-white">
-        <ul className="flex flex-col xl:flex-row xl:justify-between">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <main className="absolute w-full h-full pt-24 flex flex-col items-center">
-        <div className="w-full xl:w-4/6">
-            <Outlet />
-        </div>
-
-      </main>
-      
+    <div>
+      <Outlet />
     </div>
   )
 };
--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form"
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  };
+}
+const items = [
+  getItem('Option 1', '1', <PieChartOutlined />),
+  getItem('Option 2', '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
 
 
 export default function Dashboard() {
+
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
   const {
     register,
@@ -16,19 +49,89 @@ export default function Dashboard() {
   console.log(watch("example"))
 
   return (
-    <main>
-          <form onSubmit={handleSubmit(onSubmit)}>
+    <Layout
+      style={{
+        minHeight: '100vh',
+      }}
+    >
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        />
+        <Content
+          style={{
+            margin: '0 16px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  )
+}
+
+/*
+
+<main>
+          <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col'>
             <label htmlFor="title">Title</label>
-            <input id='title' placeholder='this is a title' {...register("title", {required: true})} />
+            <input 
+              id='title' 
+              placeholder='necessitatibus, esse aperiam velit nesciunt, hic dignissimos deserunt.' 
+              {...register("title", {required: true})} 
+            />
             {errors.title && <span>This field is required</span>}
+
+            <label htmlFor="description">Description</label>
+            <textarea 
+              id='description' 
+              placeholder='Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Possimus voluptatum quia provident nisi iure quae' 
+              {...register("description", { required: true })}
+            >
+            </textarea>
+            {errors.description && <span>This field is required</span>}
 
             <label htmlFor="feature">Feature</label>
             <input id='feature' {...register("feature", { required: true })} />
             {errors.feature && <span>This field is required</span>}
 
-            <label htmlFor="feature">Tag</label>
-            <input id='feature' {...register("tag", { required: true })} />
+            <label htmlFor="tag">Tag</label>
+            <input id='tag' placeholder='drugs' {...register("tag", { required: true })} />
             {errors.tag && <span>This field is required</span>}
+
+            
 
             <button type="submit">
               submit
@@ -36,5 +139,4 @@ export default function Dashboard() {
           </form>
  
     </main>
-  )
-}
+    */ 
